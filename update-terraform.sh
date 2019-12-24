3b73e66ffffb9441d3ee945cc1ccbefb194b802e
ut-ymmt <ut08.ymmt11@gmail.com>
2019-12-24 16:42:45 +0900

    Update provider and terraform with tfupdate

--- update-terraform.sh
+++ update-terraform.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -eu
+
 VERSION=$(tfupdate release latest hashicorp/terraform)
 UPDATE_MESSAGE="[tfupdate] Update terraform to v${VERSION}"
 if hub pr list -s "open" -f "%t: %U%n" | grep -F "$UPDATE_MESSAGE"; then
@@ -7,13 +9,13 @@ if hub pr list -s "open" -f "%t: %U%n" | grep -F "$UPDATE_MESSAGE"; then
 elif hub pr list -s "merged" -f "%t: %U%n" | grep -F "$UPDATE_MESSAGE"; then
   echo "A pull request is already merged"
 else
-  git checkout -b update-terraform-to-v${VERSION} origin/master
+  git checkout -b update-terraform-to-v${VERSION} origin/${BASE_BRANCH}
   tfupdate terraform -v ${VERSION} -r ./
   if git add . && git diff --cached --exit-code --quiet; then
     echo "No changes"
   else
     git commit -m "$UPDATE_MESSAGE"
     PULL_REQUEST_BODY="For details see: https://github.com/hashicorp/terraform/releases"
-    git push origin HEAD && hub pull-request -m "$UPDATE_MESSAGE" -m "$PULL_REQUEST_BODY" -b master
+    git push origin HEAD && hub pull-request -m "$UPDATE_MESSAGE" -m "$PULL_REQUEST_BODY" -b "$BASE_BRANCH"
   fi
 fi
